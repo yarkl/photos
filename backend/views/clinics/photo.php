6f74bd061aebfb085b632e82546d9082d5c018c2
yrslove <yrslovee@gmail.com>
2017-12-08 14:56:54 +0200

    photo.php changes

--- backend/views/clinics/photo.php
+++ backend/views/clinics/photo.php
@@ -1,15 +1,15 @@
 <?php
 use dosamigos\fileupload\FileUploadUI;
-
+use yii\web\View;
 // with UI
 ?>
 <p>
     <?= \yii\helpers\Html::a('Add Photos', ['view', 'id' => $model->id], ['class' => 'btn btn-danger btn-lg']) ?>
-    
+    <h1 id="slow-connection"></h1>
     <?php \yii\widgets\Pjax::begin(); ?>
     <?= \yii\helpers\Html::beginForm(['clinics/photo', 'id' => $model->id], 'post', ['data-pjax' => '', 'class' => 'form-inline']); ?>
     <?= \yii\helpers\Html::input('text', 'string', Yii::$app->request->post('string'), ['class' => 'form-control']) ?>
-    <?= \yii\helpers\Html::submitButton('Resize', ['class' => 'btn btn-lg btn-primary', 'name' => 'hash-button']) ?>
+    <?= \yii\helpers\Html::submitButton('Resize', ['id'=> "wait", 'class' => 'btn btn-lg btn-primary', 'name' => 'hash-button']) ?>
     <?= \yii\helpers\Html::endForm() ?>
     <h3><?php echo $stringSuccess ?></h3>
     <?php \yii\widgets\Pjax::end(); ?>
@@ -40,4 +40,10 @@ FileUploadUI::widget([
 ]);
 
 ?>
+
+<?php $this->registerJs(
+"$('#wait').on('click', function() { $('#slow-connection').html('Wait free trial connection is very slow') });",
+View::POS_READY,
+'my-button-handler'
+);?>
 
